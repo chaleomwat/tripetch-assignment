75ddbe1c08ebf719211c54caec89c6c3cd01b309
chaleomwat <chaleomwat.p@gmail.com>
2023-05-08 10:21:36 +0700

    tripetch frontend assignment with next.js

--- app/page.js
+++ app/page.js
@@ -1,95 +1,198 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
 
-export default function Home() {
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+const MobileComponent = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: false,
+    prevArrow: false
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="main">
+      <section className="athlets">
+        <div className="athlets__image"></div>
+        <div className="athlets__first">
+          <div className="athlets__container">
+            <div className="athlets__group">
+              <div className="text__title">ATHLETS</div>
+              <div className="subtitle_group">
+                <span className="text__number--default">01</span>
+                <span className="text__subtitle">CONNECTION</span>
+              </div>
+              <div className="text__detail--black">
+                Connect with coaches directly, you can ping coaches to view
+                profile.
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="athlets__second">
+          <div className="athlets__container">
+            <div className="athlets__group">
+              <div className="subtitle_group">
+                <span className="text__number--default">02</span>
+                <span className="text__subtitle">COLLABORATION</span>
+              </div>
+              <div className="text__detail--black">
+                Work with other student athletes to  increase visability. When
+                you share and like other players videos it will increase your
+                visability as a player. This is the team work aspect to Surface
+                1.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="athlets__third">
+          <div className="athlets__container">
+            <div className="athlets__group">
+              <div className="subtitle_group">
+                <span className="text__number--white">03</span>
+                <span className="text__subtitle">GROWTH</span>
+              </div>
+              <div className="text__detail--white">
+                Resources and tools for you to get better as a student Athelte.
+                Access to training classes, tutor sessions, etc 
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="slide">
+          <Slider {...settings}>
+            <div>
+              <div className="slide__group">
+                <div className="slide__title">
+                  <span className="text__number--default">01</span>
+                  <span className="text__subtitle">CONNECTION</span>
+                </div>
+                <div className="slide__detail">
+                  Connect with coaches directly, you can ping coaches to view
+                  profile.
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="slide__group">
+                <div className="slide__title">
+                  <span className="text__number--default">02</span>
+                  <span className="text__subtitle">COLLABORATION</span>
+                </div>
+                <div className="slide__detail">
+                  Work with other student athletes to  increase visability. When
+                  you share and like other players videos it will increase your
+                  visability as a player. This is the team work aspect to
+                  Surface 1.
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="slide__group">
+                <div className="slide__title">
+                  <span className="text__number--default">03</span>
+                  <span className="text__subtitle">GROWTH</span>
+                </div>
+                <div className="slide__detail">
+                  Resources and tools for you to get better as a student
+                  Athelte. Access to training classes, tutor sessions, etc 
+                </div>
+              </div>
+            </div>
+          </Slider>
+        </div>
+      </section>
+      <section className="player">
+        <div className="player__image"></div>
+        <div className="player__first">
+          <div className="player__container">
+            <div className="player__group">
+              <div className="text__title">PLAYERS</div>
+              <div className="subtitle_group">
+                <span className="text__number--default">01</span>
+                <span className="text__subtitle">CONNECTION</span>
+              </div>
+              <div className="text__detail--black">
+                Connect with talented athlete directly, you can watch their
+                skills through video showreels directly from Surface 1.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="player__second">
+          <div className="player__container">
+            <div className="player__group">
+              <div className="subtitle_group">
+                <span className="text__number--default">02</span>
+                <span className="text__subtitle">COLLABORATION</span>
+              </div>
+              <div className="text__detail--black">
+                Work with recruiter to increase your chances of finding talented
+                athlete.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="player__third">
+          <div className="player__container">
+            <div className="player__group">
+              <div className="subtitle_group pt-87">
+                <span className="text__number--whitepurple">03</span>
+                <span className="text__subtitle">GROWTH</span>
+              </div>
+              <div className="text__detail--white pt-21 pb-95">
+                Save your time, recruit proper athlets for your team.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="slide">
+          <Slider {...settings}>
+            <div>
+              <div className="slide__group">
+                <div className="slide__title">
+                  <span className="text__number--default">01</span>
+                  <span className="text__subtitle">CONNECTION</span>
+                </div>
+                <div className="slide__detail">
+                  Connect with talented athlete directly, you can watch their
+                  skills through video showreels directly from Surface 1.
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="slide__group">
+                <div className="slide__title">
+                  <span className="text__number--default">02</span>
+                  <span className="text__subtitle">COLLABORATION</span>
+                </div>
+                <div className="slide__detail">
+                  Work with recruiter to increase your chances of finding
+                  talented athlete.
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="slide__group">
+                <div className="slide__title">
+                  <span className="text__number--default">03</span>
+                  <span className="text__subtitle">GROWTH</span>
+                </div>
+                <div className="slide__detail">
+                  Save your time, recruit proper athlets for your team.
+                </div>
+              </div>
+            </div>
+          </Slider>
+        </div>
+      </section>
     </main>
   )
 }
+
+export default MobileComponent
